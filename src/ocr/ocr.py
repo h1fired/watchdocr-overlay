67d01e58c55085754155c02f25ad3b834f3d8db3
h1fired <misha01213@gmail.com>
2025-11-02 14:13:47 +0200

    Clean up

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -1,4 +1,4 @@
-from src.ocr.backends import OcrBackendManager
+from src.ocr.backends import OcrBackendManager, OcrStatus
 from src.ocr.backends.gemini import GeminiOcrBackend
 from src.ocr.backends.tesseract import TesseractOcrBackend
 from src.ocr.filtering import OCRImageFilter, OCRImageOptimizer
@@ -20,8 +20,8 @@ class OCR:
         OCRImageOptimizer.optimize_size(image, config.MAX_IMAGE_RESOLUTION)
         adjusted_image = OCRImageFilter.adjust(image)
         backend = self._backends.current()
-        raw_text = backend.recognize(adjusted_image)
-        return raw_text
+        response = backend.recognize(adjusted_image)
+        return response
 
     def backends(self):
         return self._backends
--- src/translator/translate.py
+++ src/translator/translate.py
@@ -1,4 +1,4 @@
-from .backend import TranslationBackendManager
+from .backend import TranslationBackendManager, TranslationStatus
 from .backend.deepl import DeeplTranslationBackend
 from .backend.google import GoogleTranslationBackend
 
