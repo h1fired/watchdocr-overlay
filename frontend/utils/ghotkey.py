bbaa87ac966e80e79425a4f245723347e9229639
h1fired <misha01213@gmail.com>
2026-06-29 17:42:06 +0300

    (BETA) Fixed ghotkey module

--- frontend/utils/ghotkey.py
+++ frontend/utils/ghotkey.py
@@ -1,141 +1,130 @@
-import ctypes
-import ctypes.wintypes
-import atexit
-import threading
 from typing import Callable
 from collections import defaultdict
+from ctypes import wintypes
+import ctypes
+import atexit
+import threading
+import queue
 
 
 user32 = ctypes.windll.user32
-kernel32 = ctypes.windll.kernel32
 
 
 WH_KEYBOARD_LL = 13
 WM_KEYDOWN = 0x0100
-WM_KEYUP = 0x0101
 WM_SYSKEYDOWN = 0x0104
+WM_KEYUP = 0x0101
 WM_SYSKEYUP = 0x0105
+
 VK_SHIFT = 0x10
 VK_CONTROL = 0x11
-VK_LCONTROL = 0xA2
-VK_RCONTROL = 0xA3
 VK_MENU = 0x12
-VK_LSHIFT = 0xA0
-VK_RSHIFT = 0xA1
-VK_CAPITAL = 0x14
 
 
 HOOKPROTYPE = ctypes.CFUNCTYPE(
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.c_int,
-    ctypes.POINTER(ctypes.c_void_p)
+    ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_void_p)
 )
 
 
 class KBDLLHOOKSTRUCT(ctypes.Structure):
     _fields_ = [
-        ("vkCode", ctypes.wintypes.DWORD),
-        ("scanCode", ctypes.wintypes.DWORD),
-        ("flags", ctypes.wintypes.DWORD),
-        ("time", ctypes.wintypes.DWORD),
-        ("dwExtraInfo", ctypes.wintypes.LPVOID),
+        ("vkCode", wintypes.DWORD),
+        ("scanCode", wintypes.DWORD),
+        ("flags", wintypes.DWORD),
+        ("time", wintypes.DWORD),
+        ("dwExtraInfo", wintypes.LPVOID),
     ]
-
-
-def _uninstallHookProc(hooked):
-    if hooked is None:
-        return
-    user32.UnhookWindowsHookEx(hooked)
-
-
-def _installHookProc(pointer):
-    return user32.SetWindowsHookExA(
-        WH_KEYBOARD_LL,
-        pointer,
-        None,
-        0
-    )
 
 
 context = {
     'e': threading.Event(),
     'hotkeys': defaultdict(list),
-    'pressed_keys': set()
+    'pressed_keys': set(),
+    'hook': None,
+    'running': True
 }
+event_queue = queue.Queue()
+
+
+def _process_callbacks():
+    while context['running']:
+        try:
+            # Wait for keystrokes from the hook
+            keys = event_queue.get(timeout=0.5)
+            if not keys:
+                continue
+
+            for shortcut, callbacks in context['hotkeys'].items():
+                s_shortcut = set(shortcut.split('+'))
+                if s_shortcut.issubset(keys):
+                    for cb in callbacks:
+                        cb()
+        except queue.Empty:
+            continue
 
 
 def _hookProc(nCode, wParam, lParam):
-    if nCode < 0:
-        return user32.CallNextHookEx(context['hook'], nCode, wParam, lParam)
-    else:
-        if wParam == WM_KEYDOWN or wParam == WM_SYSKEYDOWN:
-            keys = set()
+    if nCode >= 0:
+        kb = ctypes.cast(lParam, ctypes.POINTER(KBDLLHOOKSTRUCT)).contents
+        vk = kb.vkCode
 
-            kb = ctypes.cast(lParam, ctypes.POINTER(KBDLLHOOKSTRUCT)).contents
-            vk = kb.vkCode
-            scan = kb.scanCode
-            key_state = (ctypes.c_ubyte * 256)()
-            user32.GetKeyboardState(ctypes.byref(key_state))
-            result = ctypes.c_uint()
-            if user32.ToAscii(vk, scan, key_state, ctypes.byref(result), 0):
-                keys.add(chr(result.value).upper())
+        if wParam in (WM_KEYDOWN, WM_SYSKEYDOWN):
+            if vk not in context['pressed_keys']:
+                context['pressed_keys'].add(vk)
 
-            # Retrieve modifiers
-            if (
-                user32.GetAsyncKeyState(VK_LSHIFT) & 0x8000
-                or user32.GetAsyncKeyState(VK_RSHIFT) & 0x8000
-            ):
-                keys.add('Shift')
-            if (
-                user32.GetKeyState(VK_LCONTROL) < 0
-                or user32.GetKeyState(VK_RCONTROL) < 0
-            ):
-                keys.add('Ctrl')
-            if user32.GetKeyState(VK_MENU) & 0x8000:
-                keys.add('Alt')
+                keys = set()
 
-            # Avoid auto repeat
-            if kb.vkCode not in context['pressed_keys']:
-                _callback(keys)
-                context['pressed_keys'].add(kb.vkCode)
-        elif wParam == WM_KEYUP or wParam == WM_SYSKEYUP:
-            kb = ctypes.cast(lParam, ctypes.POINTER(KBDLLHOOKSTRUCT)).contents
-            if kb.vkCode in context['pressed_keys']:
-                context['pressed_keys'].remove(kb.vkCode)
+                char_code = user32.MapVirtualKeyA(vk, 2)
+                if char_code:
+                    keys.add(chr(char_code & 0xFF).upper())
+
+                # Use GetAsyncKeyState for true asynchronous hardware state
+                if user32.GetAsyncKeyState(VK_SHIFT) & 0x8000:
+                    keys.add('Shift')
+                if user32.GetAsyncKeyState(VK_CONTROL) & 0x8000:
+                    keys.add('Ctrl')
+                if user32.GetAsyncKeyState(VK_MENU) & 0x8000:
+                    keys.add('Alt')
+
+                # Offload to the queue immediately. Do NOT run callbacks here.
+                event_queue.put(keys)
+
+        elif wParam in (WM_KEYUP, WM_SYSKEYUP):
+            if vk in context['pressed_keys']:
+                context['pressed_keys'].remove(vk)
 
     return user32.CallNextHookEx(context['hook'], nCode, wParam, lParam)
 
 
-def _callback(keys: set[str]):
-    if not keys:
-        return
-
-    for shortcut, callbacks in context['hotkeys'].items():
-        s_shortcut = set(shortcut.split('+'))
-        if s_shortcut.issubset(keys):
-            for cb in callbacks:
-                cb()
-
-
 def _install_keyboard_hook_proc():
     context['pointer'] = HOOKPROTYPE(_hookProc)
-    hooked = _installHookProc(context['pointer'])
-    context['hook'] = hooked
+    context['hook'] = user32.SetWindowsHookExA(WH_KEYBOARD_LL, context['pointer'], None, 0)
+
     context['e'].set()
-    if hooked:
-        msg = ctypes.wintypes.MSG()
-        while user32.GetMessageA(ctypes.byref(msg), 0, 0, 0) != 0:
+
+    if context['hook']:
+        msg = wintypes.MSG()
+        # Windows requires a message pump to keep the hook alive
+        while user32.GetMessageA(ctypes.byref(msg), 0, 0, 0) > 0:
             user32.TranslateMessage(ctypes.byref(msg))
             user32.DispatchMessageA(ctypes.byref(msg))
 
 
+def _uninstallHookProc():
+    context['running'] = False
+    if context['hook']:
+        user32.UnhookWindowsHookEx(context['hook'])
+
+
 def install_keyboard_hook_proc():
-    th = threading.Thread(target=_install_keyboard_hook_proc)
-    th.daemon = True
+    processor_thread = threading.Thread(target=_process_callbacks, daemon=True)
+    processor_thread.start()
+
+    th = threading.Thread(target=_install_keyboard_hook_proc, daemon=True)
     th.start()
+
     context['e'].wait()
-    atexit.register(lambda: _uninstallHookProc(context['hook']))
+    atexit.register(_uninstallHookProc)
     return bool(context['hook'])
 
 
@@ -144,7 +133,8 @@ def bind(shortcut: str, callback: Callable):
 
 
 def unbind(shortcut: str, callback: Callable):
-    context['hotkeys'][shortcut].remove(callback)
+    if callback in context['hotkeys'][shortcut]:
+        context['hotkeys'][shortcut].remove(callback)
 
 
 def unbind_all():
