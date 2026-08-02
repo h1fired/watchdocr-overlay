6234c1e03b12c86c99c7cefbd80ef7f5d68f53f2
h1fired <misha01213@gmail.com>
2026-08-02 18:15:56 +0300

    Added mask creation method for text remover

--- src/watchdocr/processor/inpaint.py
+++ src/watchdocr/processor/inpaint.py
@@ -2,6 +2,8 @@ from src.common.plugin import PluginManager
 from src.watchdocr.plugins.image.textremover import ImageTextRemoverPlugin
 from src.common.utils.logging import log
 from PIL import Image
+import numpy as np
+import cv2
 
 
 class ImageTextRemover:
@@ -17,4 +19,13 @@ class ImageTextRemover:
             )
             raise ValueError('Text remover backend plugins not found')
         api = sorted(apis, key=lambda e: e.get_priority())[0]
-        return api.filter_image(image)
+
+        mask = self._create_mask(image.width, image.height, boxes)
+
+        return api.filter_image(image, mask)
+
+    def _create_mask(self, width: int, height: int, boxes: tuple):
+        mask = np.zeros((height, width), dtype=np.uint8)
+        for x, y, w, h in boxes:
+            cv2.rectangle(mask, (x, y), (x + w, y + h), 255, thickness=-1)
+        return Image.fromarray(mask)
